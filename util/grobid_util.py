81df5e52b30cb3a3700a1471deb988a9b5d6e561
Aye10032 <aye10032@gmail.com>
2024-08-06 16:57:41 +0800

    添加pdf的进一步解析

---
+++ util/grobid_util.py
@@ -0,0 +1,56 @@
+import re
+
+from bs4 import BeautifulSoup
+from grobid_client.grobid_client import GrobidClient
+
+
+def parse_pdf(pdf_path: str) -> str:
+    client = GrobidClient(config_path='../grobid.json')
+    result = client.process_pdf(
+        "processFulltextDocument",
+        pdf_path,
+        False,
+        True,
+        False,
+        False,
+        False,
+        False,
+        False
+    )
+
+    return result[2]
+
+
+def check_title(title):
+    pattern = re.compile(r'^(Introduction|Discussions?)$', re.IGNORECASE)
+    return bool(pattern.match(title))
+
+
+def extract_paragraphs(xml: str) -> dict:
+    soup = BeautifulSoup(xml, 'xml')
+    paragraphs = soup.find('body').find_all('div', recursive=False)
+
+    result = {}
+    for paragraph in paragraphs:
+        if paragraph.find('head') is None:
+            continue
+
+        title = paragraph.find('head').text.strip()
+        if check_title(title):
+
+            text_list = []
+            for p in paragraph.find_all('p'):
+                text_list.append(p.text.strip())
+
+            result[title] = '\n'.join(text_list)
+
+    return result
+
+
+def main() -> None:
+    xml_str = parse_pdf("../test/2024.07.31.606043v1.full.pdf")
+    extract_paragraphs(xml_str)
+
+
+if __name__ == '__main__':
+    main()
