86673ddf03df19a36fe5a49ec110ddc18cbb4a09
Aye10032 <aye10032@gmail.com>
2024-08-07 23:13:04 +0800

    调整prompt

--- util/llm_integration.py
+++ util/llm_integration.py
@@ -13,7 +13,8 @@ SYSTEM_PROMPT = """You are an expert research assistant specializing in summariz
 Your task is to provide a concise and comprehensive summary of a given academic paper."""
 
 ASK_PROMPT = """下面是文献的相关信息：\n{info}\n\n请用中文以简洁的语言给出的文献内容进行总结，同时务必包含文献真正关键的信息。
-尽可能不超过300字，若内容非常多，不超过500字。在最后，用一句话总结这篇文献的意义，以“这项研究...”开头。"""
+尽可能不超过300字，若内容非常多，不超过500字，以“来自XXX的这项研究工作...”或“来自XXX的研究团队在这篇文章中...”或“来自XXX的研究人员提出了...”类似的句子开头。
+在最后，另起一行用一句话总结这篇文献的意义，以“这项研究...”开头。"""
 
 
 def load_gpt() -> ChatOpenAI:
@@ -29,7 +30,6 @@ def load_gpt() -> ChatOpenAI:
 def format_paper(paper: Paper) -> str:
     formatted_str = (
         f"Title: {paper.title}\n"
-        f"Authors: {paper.authors}\n"
         f"Institution: {paper.author_corresponding_institution}\n"
         f"# Abstract\n {paper.abstract}"
     )
