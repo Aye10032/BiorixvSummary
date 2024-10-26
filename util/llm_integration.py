8f672b59b8bb99df0aa1400617946cc9496350b7
Aye10032 <tc_aye@outlook.com>
2024-10-26 13:59:18 +0800

    update prompt

--- util/llm_integration.py
+++ util/llm_integration.py
@@ -9,14 +9,14 @@ from langchain_openai import ChatOpenAI
 
 from util.biorxiv_fetcher import Paper
 
-SYSTEM_PROMPT = """You are an expert research assistant specializing in summarizing academic papers.
-Your task is to provide a concise and comprehensive summary of a given academic paper."""
+SYSTEM_PROMPT = """你是一名科研助理，你的任务是对于用户给出的科研文献内容进行精炼总结，总结时需要遵从以下格式：
+你给出的总结总共分为两段，600字以内。
+第一段以以“来自XXX的这项研究工作...”或“来自XXX的研究团队在这篇文章中...”或“来自XXX的研究人员提出了...”类似的句子开头。这一段的总结内容包括文章的创新点、主要内容、取得的主要成果、作者认为未来要做的工作（如果有）。
+第二段以“这项研究...”开头，用一句话总结一下这篇文章的意义。"""
 
 ASK_PROMPT = """下面是文献的相关信息：
 \n==================\n{info}\n====================\n
-请用中文以简洁的语言给出的文献内容进行总结，务必包含文献真正关键的信息。尽可能不超过400字，若内容非常多，不超过600字。
-首先用一到两段概括全文的主要内容，以“来自XXX的这项研究工作...”或“来自XXX的研究团队在这篇文章中...”或“来自XXX的研究人员提出了...”类似的句子开头。这部分不用包含对与研究意义的概括。
-在最后，另起一行用一句话总结这篇文献的意义，以“这项研究...”开头。"""
+请用中文以简洁的语言给出的文献内容进行总结，，符合要求的格式，务必包含文献真正关键的信息。"""
 
 
 def load_gpt() -> ChatOpenAI:
